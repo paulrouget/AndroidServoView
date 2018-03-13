11dece5b545484db2b4b3797754a932857ee7b47
Paul Rouget <me@paulrouget.com>
2018-03-13 11:35:36 +0100

    format

--- geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -7,31 +7,31 @@ import android.widget.TextView;
 import android.view.ViewGroup;
 
 public class GeckoView  extends FrameLayout {
-    public GeckoView(final Context context) {
-        super(context);
-    }
+  public GeckoView(final Context context) {
+    super(context);
+  }
 
-    private TextView mView;
-    public GeckoView(final Context context, final AttributeSet attrs) {
-        super(context, attrs);
-        setFocusable(true);
-        setFocusableInTouchMode(true);
-        setDescendantFocusability(FOCUS_BLOCK_DESCENDANTS);
-        setWillNotCacheDrawing(false);
+  private TextView mView;
+  public GeckoView(final Context context, final AttributeSet attrs) {
+    super(context, attrs);
+    setFocusable(true);
+    setFocusableInTouchMode(true);
+    setDescendantFocusability(FOCUS_BLOCK_DESCENDANTS);
+    setWillNotCacheDrawing(false);
 
-        mView = new TextView(getContext());
-        addView(mView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-	mView.append("INIT\n");
-    }
+    mView = new TextView(getContext());
+    addView(mView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    mView.append("INIT\n");
+  }
 
 
-    private GeckoSession mSession;
+  private GeckoSession mSession;
 
-    public void setSession(GeckoSession session) {
-	    mSession = session;
-    }
+  public void setSession(GeckoSession session) {
+    mSession = session;
+  }
 
-    public GeckoSession getSession() {
-        return mSession;
-    }
+  public GeckoSession getSession() {
+    return mSession;
+  }
 }
